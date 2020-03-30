e27a7cf3252ef1b7d2a7cbb3d8ff7e2236a34376
Marcin Jaroszewski <marcin.jaroszewski@dcx.bz>
2020-03-30 19:01:44 +0200

    add heloo name

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
     return {"message": "Hello_world"}
+
+
+@app.get('/hello/{name}')
+def hello_name(name: str):
+    return {"message": f"Hello {name}"}
